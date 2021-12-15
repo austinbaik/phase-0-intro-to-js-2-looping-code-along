92485933d06045641bacb86777c10341b5b431ea
Austin Baik <baik.austin@gmail.com>
2021-12-14 17:55:54 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+function writeCards(names, event){
+
+    let printCard =[]
+
+    for (let i = 0; i < names.length; i++){
+
+        printCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+return printCard;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+
+
+function countDown(number) {
+    while (number >= 0){
+        console.log(number--);
+    }
+
+}
+
+
